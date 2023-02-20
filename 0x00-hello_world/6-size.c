1c083ca06649e19d23ac2d1686c87e1dded06b3d
Farouk Ashraf <faroukashrafelshamy321@gmail.com>
2023-02-21 01:15:08 +0200

    new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long int thelong;
 long long int thell;
 float thefloat;
 printf("Size of a char: %zu byte(s)\n", sizeof(thechar));
-printf("Size of a int: %zu byte(s)\n", sizeof(theint));
+printf("Size of an int: %zu byte(s)\n", sizeof(theint));
 printf("Size of a long int: %zu byte(s)\n", sizeof(thelong));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(thell));
 printf("Size of a float: %zu byte(s)\n", sizeof(thefloat));
