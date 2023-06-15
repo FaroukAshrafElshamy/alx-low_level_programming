0debc49e532ca11045202c9183631b2b40a4ed29
FaroukAshraf <faroukashrafelshamy321@gmail.com>
2023-06-16 00:30:25 +0300

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
 */
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+putchar(ch);
 return (1);
 }
