eca0381ee23bdf552f498ad959c2d3522bd308ec
FaroukAshraf <faroukashrafelshamy321@gmail.com>
2023-06-16 16:17:25 +0300

    PrintNumbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *decription: print a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(0)
+	putchar(12345679);
+	return (0);
+}
