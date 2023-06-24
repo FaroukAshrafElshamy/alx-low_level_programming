bc0d176934076de1dec6489906d510a37d732f80
FaroukAshraf <faroukashrafelshamy321@gmail.com>
2023-06-24 04:13:57 -0400

    More Functin and nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * more_numbers - print 0 - 14 ten times and you
- *		can only use _putchar three times
+ *		can only use putchar three times
  *
  * Return: Always 0 (Success)
 */
@@ -20,11 +20,11 @@ void more_numbers(void)
 			num = count;
 			if (count > 9)
 			{
-				_putchar(1 + 48);
+				putchar(1 + 48);
 				num = count % 10;
 			}
-			_putchar(num + 48);
+			putchar(num + 48);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
