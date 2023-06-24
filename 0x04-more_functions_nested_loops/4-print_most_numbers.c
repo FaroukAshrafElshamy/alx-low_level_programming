2263cdb9013a7c07c62e57067e1e110758eca9db
FaroukAshraf <faroukashrafelshamy321@gmail.com>
2023-06-24 04:10:03 -0400

    Print Most Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; ++num)
 	{
 		if (num != 2 && num != 4)
-			_putchar(num + 48);
+			putchar(num + 48);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
