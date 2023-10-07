2622f6624755745e53383fb771356d101eb1d099
Farouk Ashraf <104506576+FaroukAshrafElshamy@users.noreply.github.com>
2023-10-07 19:39:23 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,32 @@
 #include "main.h"
-
 /**
- * create_file - reads a text file and prints it to the POSIX standard output
+ * create_file -creates an array of chars, and initializes
  *
- * @filename: name of the file to be read
- * @text_content: a NULL terminated string to write to the file
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
  *
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int wc1 = 0, wc2, fp;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fp < 0)
-		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		while (text_content[wc1])
-			wc1++;
-
-		wc2 = write(fp, text_content, wc1);
-		if (wc2 != wc1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-close(fp);
-return (1);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
