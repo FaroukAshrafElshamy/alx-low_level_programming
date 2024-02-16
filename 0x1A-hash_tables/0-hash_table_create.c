213ea79cc6d8237690cfb19c134d8310714c5c60
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-16 22:13:43 +0200

    ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 /**
 * hash_table_create - create a hash table
 *@size: size of the array
-*Reaturn: pointer to the newly created hash table
+*Return: pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
