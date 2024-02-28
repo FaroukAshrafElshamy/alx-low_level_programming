9a5c429ab26bbc115a652c9776c156467730d2d6
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-28 19:07:52 +0200

    Putchar

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * _putchar - write the characters to stdout
+ * @c: character to print
+ * Return: 1 on success else -1
+ */
+
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
